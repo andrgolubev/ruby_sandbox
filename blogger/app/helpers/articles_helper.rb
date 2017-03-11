96f901499642a9249a2b5b5f468540a1f266e5e3
andrgolubev <andpgolubev@gmail.com>
2017-03-11 22:06:51 +0300

    Added tags support

--- blogger/app/helpers/articles_helper.rb
+++ blogger/app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def article_params #strong parameters for article hash
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 end
